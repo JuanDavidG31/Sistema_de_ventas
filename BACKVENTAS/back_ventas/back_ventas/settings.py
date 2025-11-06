1481859ae10d8bc4e8cf9854418f848926c1fa06
JuanDavidG31 <jdgonzalezh@unbosque.edu.co>
2025-11-05 21:40:59 -0500

    Solucion problemas django, usar version 1.3.6

--- BACKVENTAS/back_ventas/back_ventas/settings.py
+++ BACKVENTAS/back_ventas/back_ventas/settings.py
@@ -79,11 +79,15 @@ WSGI_APPLICATION = 'back_ventas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'juguetes', 
-        'CLIENT': {
-            'host': config('DATABASE_URL'),
-        }
+        
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        
+         'ENGINE': 'djongo',
+         'NAME': 'juguetes', 
+         'CLIENT': {
+             'host': config('DATABASE_URL'),
+         }
     }
 }
 
