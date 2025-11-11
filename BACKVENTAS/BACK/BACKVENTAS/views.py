a8dc5d4eb8d7f15d6731ce96ac765fa9cec3f220
JuanDavidG31 <jdgonzalezh@unbosque.edu.co>
2025-11-10 22:01:33 -0500

    quitar JWT

--- BACKVENTAS/BACK/BACKVENTAS/views.py
+++ BACKVENTAS/BACK/BACKVENTAS/views.py
@@ -16,49 +16,49 @@ from .serializers import (
 class DepartamentoViewSet(viewsets.ModelViewSet):
     queryset = Departamento.objects.all()
     serializer_class = DepartamentoSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    #permission_classes = [permissions.IsAuthenticated]
 
 class MunicipioViewSet(viewsets.ModelViewSet):
     queryset = Municipio.objects.all()
     serializer_class = MunicipioSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    #permission_classes = [permissions.IsAuthenticated]
 
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    #permission_classes = [permissions.IsAuthenticated]
 
 # --- ViewSets de Clientes y Productos ---
 
 class ClienteViewSet(viewsets.ModelViewSet):
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    #permission_classes = [permissions.IsAuthenticated]
 
 class LineaProductoViewSet(viewsets.ModelViewSet):
     queryset = LineaProducto.objects.all()
     serializer_class = LineaProductoSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    #permission_classes = [permissions.IsAuthenticated]
 
 class ProductoViewSet(viewsets.ModelViewSet):
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    #permission_classes = [permissions.IsAuthenticated]
 
 class ProductoImagenViewSet(viewsets.ModelViewSet):
     queryset = ProductoImagen.objects.all()
     serializer_class = ProductoImagenSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    #permission_classes = [permissions.IsAuthenticated]
 
 # --- ViewSets de Venta ---
 
 class VentaDetalleViewSet(viewsets.ModelViewSet):
     queryset = VentaDetalle.objects.all()
     serializer_class = VentaDetalleSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    #permission_classes = [permissions.IsAuthenticated]
 
 class VentaViewSet(viewsets.ModelViewSet):
     
     queryset = Venta.objects.prefetch_related('detalles').all()
     serializer_class = VentaSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    #permission_classes = [permissions.IsAuthenticated]
